9ac293d0ad36a16729301eb7bbc7960adeb444e5
Anthony Kim <fbssaejun@gmail.com>
2021-08-19 01:25:36 +0000

    returns promise with sql query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,8 +1,18 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+
+
 
 /// Users
-
 /**
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
@@ -67,11 +77,12 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  return pool
+  .query(`SELECT * FROM properties LIMIT $1`, [limit])
+  .then((res) => res.rows)
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.getAllProperties = getAllProperties;
 
